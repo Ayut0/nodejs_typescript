3515d2e7ad87c4527c0f71616035b7d09862143f
Yuto Yama <yuvto.0420@gmail.com>
2023-09-29 21:14:17 -0700

    Organize dir structure

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -9,21 +9,24 @@ router.get("/", (req, res, next) => {
 });
 // create
 router.post("/todo", (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     return res.status(200).json({ message: "Todo added", todos: todos });
 });
 // update
 router.put("/todo/:todoId", (req, res, next) => {
-    const todoId = req.params.todoId;
+    const params = req.params;
+    const todoId = params.todoId;
+    const body = req.body;
     const todoIndex = todos.findIndex((todoItem) => todoItem.id === todoId);
     if (todoIndex >= 0) {
         todos[todoIndex] = {
             id: todos[todoIndex].id,
-            text: req.body.text,
+            text: body.text,
         };
         return res.status(200).json({ message: "Todo updated", todos: todos });
     }
@@ -31,7 +34,8 @@ router.put("/todo/:todoId", (req, res, next) => {
 });
 // delete
 router.delete("/todo/:todoId", (req, res, next) => {
-    const todoId = req.params.todoId;
+    const params = req.params;
+    const todoId = params.todoId;
     todos = todos.filter((todoItem) => todoItem.id !== todoId);
     res.status(200).json({ message: "Todo deleted", todos: todos });
 });
